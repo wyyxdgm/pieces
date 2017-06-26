c4b8578ebaee42db86cd7917c77026b0249caf3c
guimaodai <wyyxdgm@163.com>
2017-06-26 19:10:32 +0800

    xx

---
+++ qlzg/问卷json转化默认选中.js
@@ -0,0 +1,513 @@
+qResult = {
+    "success": true,
+    "code": "NT-0000",
+    "data": [{
+        "isMultiSelect": false,
+        "qno": "00001",
+        "qtitle": "贵单位的性质：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 国有企事业单位"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 非上市民营企业"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 外资企业"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 上市公司"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00002",
+        "qtitle": "贵单位的净资产规模为：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 500万元以下"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 500万元-2000万元"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 2000万元-1亿元"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 超过1亿元"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00003",
+        "qtitle": "贵单位年营业收入为：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 500万元以下"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 500万元-2000万元"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 2000万元-1亿元"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 超过1亿元"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00004",
+        "qtitle": "贵单位证券账户资产为：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A．300万元以内"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B．300万元-1000万元"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C．1000万元-3000万元"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D．超过3000万元"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00005",
+        "qtitle": "贵单位是否有尚未清偿的数额较大的债务？如有，主要是：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 银行贷款"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 公司债券或企业债券"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 通过担保公司等中介机构募集的借款"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 民间借贷"
+        }, {
+            "itemNo": "E",
+            "itemtitle": "E. 没有数额较大的债务"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00006",
+        "qtitle": "对于金融产品投资工作，贵单位打算配置怎样的人员力量：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 一名兼职人员（包括负责人自行决策）"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 一名专职人员"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 多名兼职或专职人员，相互之间分工不明确"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 多名兼职或专职人员，相互之间有明确分工"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00007",
+        "qtitle": "贵单位所配置的负责金融产品投资工作的人员是否符合以下情况：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 现在或此前曾从事金融、经济或财会等与金融产品投资相关的工作超过两年"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 已取得金融、经济或财会等与金融产品投资相关专业学士以上学位"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C．取得证券从业资格、期货从业资格、注册会计师证书（CPA）或注册金融分析师证书（CFA）中的一项及以上"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D．本单位所配置的人员不符合以上任何一项描述"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00008",
+        "qtitle": "贵单位是否建立了金融产品投资相关的管理制度：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 没有。因为要保证操作的灵活性"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 已建立。包括了分工和授权的要求，但未包括投资风险控制的规则"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 已建立。包括了分工与授权、风险控制等一系列与金融产品投资有关的规则"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00009",
+        "qtitle": "贵单位的投资经验可以被概括为：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 有限：除银行活期账户和定期存款外，基本没有其他投资经验"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 一般：除银行活期账户和定期存款外，购买过基金、保险等理财产品，但还需要进一步的指导"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 丰富：本单位具有相当投资经验，参与过股票、基金等产品的交易，并倾向于自己做出投资决策"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 非常丰富：本单位对于投资非常有经验，参与过权证、期货或创业板等高风险产品的交易"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00010",
+        "qtitle": "有一位投资者一个月内做了15笔交易（同一品种买卖各一次算一笔），贵单位认为这样的交易频率：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 太高了"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 偏高"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 正常"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 偏低"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00011",
+        "qtitle": "过去一年时间内，您购买的不同产品或接受的不同服务（含同一类型的不同产品或服务）的数量是：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 5个以下"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 6至10个"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 11至15个"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 16个以上"
+        }]
+    }, {
+        "isMultiSelect": true,
+        "qno": "00012",
+        "qtitle": "以下金融产品或服务，贵单位投资经验在两年以上的有：（本题可多选，但评分以其中最高分值选项为准）",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 银行存款等"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 债券、货币市场基金、债券型基金或其它固定收益类产品等"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 股票、混合型基金、偏股型基金、股票型基金等权益类投资品种"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 期货、融资融券等"
+        }, {
+            "itemNo": "E",
+            "itemtitle": "E. 复杂金融产品、其他产品或服务"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00013",
+        "qtitle": "如果贵单位曾经从事过金融市场投资，在交易较为活跃的月份，平均月交易额大概是多少：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 100万元以内"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 100万元-300万元"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 300万元-1000万元"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 1000万元以上"
+        }, {
+            "itemNo": "E",
+            "itemtitle": "E. 从未投资过金融产品"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00014",
+        "qtitle": "贵单位用于证券投资的大部分资金不会用作其它用途的时间段为：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 0到1年"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 1到3年"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 3年到5年"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 5年以上"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00015",
+        "qtitle": "贵单位进行投资时的首要目标是：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 尽可能保证本金安全，不在乎收益率比较低"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 产生一定的收益，可以承担一定的投资风险"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 产生较多的收益，可以承担较大的投资风险"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 实现资产大幅增长，愿意承担很大的投资风险"
+        }]
+    }, {
+        "isMultiSelect": true,
+        "qno": "00016",
+        "qtitle": "贵单位打算重点投资于哪些种类的投资品种？（本题可多选，但评分以其中最高分值选项为准）",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 货币市场工具为主的现金管理类投资品种"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 债券型产品、偏债混合型产品等固定收益类投资品种"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 股票型产品、偏股混合型产品、一般混合型产品等权益类投资品种"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 包含期货、期权风险敞口的投资品种或复杂金融产品或服务"
+        }, {
+            "itemNo": "E",
+            "itemtitle": "E. 其他产品或服务"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00017",
+        "qtitle": "贵单位认为自己能承受的最大投资损失是多少？",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 尽可能保证本金安全"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 一定的投资损失"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 较大的投资损失"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 损失可能超过本金"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00018",
+        "qtitle": "假设有两种不同的投资：投资A预期获得5%的收益，有可能承担非常小的损失；投资B预期获得20%的收益，但有可能面临25%甚至更高的亏损。您将您的投资资产分配为：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 全部投资于A"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 大部分投资于A"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 两种投资各一半"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 大部分投资于B"
+        }, {
+            "itemNo": "E",
+            "itemtitle": "E. 全部投资于B"
+        }]
+    }, {
+        "isMultiSelect": false,
+        "qno": "00019",
+        "qtitle": "贵单位参与金融产品投资的主要目的是什么：",
+        "qitem": [{
+            "itemNo": "A",
+            "itemtitle": "A. 闲置资金保值增值"
+        }, {
+            "itemNo": "B",
+            "itemtitle": "B. 获取主营业务以外的投资收益"
+        }, {
+            "itemNo": "C",
+            "itemtitle": "C. 现货套期保值、对冲主营业务风险"
+        }, {
+            "itemNo": "D",
+            "itemtitle": "D. 减持已持有的股票"
+        }]
+    }],
+    "message": "success",
+    "extra": null
+}
+
+var qRResult = [{
+    "qno": "00001",
+    "chooseItemList": [{
+        "itemNo": "C"
+    }]
+}, {
+    "qno": "00002",
+    "chooseItemList": [{
+        "itemNo": "C"
+    }]
+}, {
+    "qno": "00003",
+    "chooseItemList": [{
+        "itemNo": "D"
+    }]
+}, {
+    "qno": "00004",
+    "chooseItemList": [{
+        "itemNo": "C"
+    }]
+}, {
+    "qno": "00005",
+    "chooseItemList": [{
+        "itemNo": "D"
+    }]
+}, {
+    "qno": "00006",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00007",
+    "chooseItemList": [{
+        "itemNo": "C"
+    }]
+}, {
+    "qno": "00008",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00009",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00010",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00011",
+    "chooseItemList": [{
+        "itemNo": "D"
+    }]
+}, {
+    "qno": "00012",
+    "chooseItemList": [{
+        "itemNo": "C"
+    }, {
+        "itemNo": "D"
+    }]
+}, {
+    "qno": "00013",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00014",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00015",
+    "chooseItemList": [{
+        "itemNo": "C"
+    }]
+}, {
+    "qno": "00016",
+    "chooseItemList": [{
+        "itemNo": "C"
+    }]
+}, {
+    "qno": "00017",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00018",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}, {
+    "qno": "00019",
+    "chooseItemList": [{
+        "itemNo": "B"
+    }]
+}];
+// qResult
+// qRResult
+qitem = [{
+    "itemNo": "A",
+    "itemtitle": "A. 闲置资金保值增值"
+}, {
+    "itemNo": "B",
+    "itemtitle": "B. 获取主营业务以外的投资收益"
+}, {
+    "itemNo": "C",
+    "itemtitle": "C. 现货套期保值、对冲主营业务风险"
+}, {
+    "itemNo": "D",
+    "itemtitle": "D. 减持已持有的股票"
+}];
+var qrMap = {}; //答案，key:qno value:[{"itemNo": "C"}]
+for (var i in qRResult) {
+    // qRResult[i] = {
+    //     "qno": "00001",
+    //     "chooseItemList": [{
+    //         "itemNo": "C"
+    //     }]
+    // }
+    var relist = qRResult[i].chooseItemList;
+    qrMap[qRResult[i].qno] = {};
+    for (var i in relist) {
+        qrMap[qRResult[i].qno][relist[i].itemNo] = true;
+    }
+}
+var qno = qno;
+$.each(qitem, function(i, item) {
+    qre = qrMap[qno]; //{ A: true, B: true } 
+    // qre[item.itemNo]?'checked="checked" diabled':'diabled'
+    ht += ("<input  name=\"risk_input" + item.qno + "\" class=\"checkinput " + (item.isMultiSelect ? 'ismo' : '') + "\" type=\"" + (item.isMultiSelect ? 'checkbox' : 'radio') + "\" style='margin-left: 20px' data-v = '" + item2.itemNo + "'/>&nbsp&nbsp" + item2.itemtitle + "<br/><br/>");
+});
+// $(function() {
+//     var riskAnswer = '${agencyRiskQuestionPage.riskAnswer}';
+//     console.info(riskAnswer)
+//     var objData = JSON.parse(riskAnswer);
+//     console.info(objData);
+//     var questionCount = 0;
+//     var riskMap = {
+
+//     };
+//     $.ajax({
+//         url: "/agencyRiskQuestionController.do?getCleanRiskQuestions",
+//         type: "POST",
+//         success: function(data) {
+//             var parseData = JSON.parse(data);
+//             console.info("题目对象:" + data)
+//             if (parseData.success == true) {
+//                 $.each(parseData.data, function(i, item) {
+//                     questionCount++;
+//                     riskMap[item.qno] = "001";
+//                     $("#risk_ul").append("<li style='border-top: 1px dotted #ddd'value='" + item.qno + "'><div style='margin-top: 20px;margin-left: 20px'><span style='font-weight: bold;font-size: 17px'>" + (i + 1) + "、" + item.qtitle + "</span><div id='risk_div'><br/></div></div></li>");
+//                     var ht = '<div>';
+//                     $.each(item.qitem, function(i, item2) {
+//                         ht += ("<input  name=\"risk_input" + item.qno + "\" class=\"checkinput " + (item.isMultiSelect ? 'ismo' : '') + "\" type=\"" + (item.isMultiSelect ? 'checkbox' : 'radio') + "\" style='margin-left: 20px' data-v = '" + item2.itemNo + "'/>&nbsp&nbsp" + item2.itemtitle + "<br/><br/>");
+//                     });
+//                     ht += '</div>';
+//                     $("#risk_ul").append(ht);
+//                 });
+//                 $('li>input').click();
+//                 $(".checkinput").click(function() {
+//                     if (!$(this).hasClass("ismo")) $(this).siblings().removeAttr('checked');
+//                 });
+//             }
+//         }
+//     });
+
+
+// })
